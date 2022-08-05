cfc0b01ff399290af01e6260848f12ba5119ae3f
Kanstantsin Asinouski <Kanstantsin_Asinouski@epam.com>
2022-08-05 13:41:37 +0200

    Changed project structure

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a5f6305-0dd7-48a8-abaa-3b5d42e5c4b6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/XMLConfigReaderTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryMarket.Classes;
+using GroceryMarket.Models;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class XMLConfigReaderTests
+    {
+        [TestMethod]
+        public void ReadCorrect()
+        {
+            XMLConfigReader reader = new XMLConfigReader();
+
+            Assert.IsNotNull(reader.Read<Product>());
+        }
+    }
+}
