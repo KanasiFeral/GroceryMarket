d6249c420103105add3a10e02538b51d8babbcbb
Kanstantsin Asinouski <Kanstantsin_Asinouski@epam.com>
2022-08-07 14:09:18 +0200

    Changed model

--- GroceryMarket/Models/Product.cs
+++ GroceryMarket/Models/Product.cs
@@ -3,13 +3,9 @@
     public class Product
     {
         public string ProductCode { get; set; }
-
         public double Price { get; set; }
-
         public double WholesalePrice { get; set; }
-
         public int WholesaleCount { get; set; }
-
         public bool IsWholesale { get; set; }
     }
 }
