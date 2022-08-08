e6a391ac899f82b9d548c726254bd59608819b20
Kanstantsin Asinouski <Kanstantsin_Asinouski@epam.com>
2022-08-08 12:07:54 +0200

    Added Stock's classes

---
+++ GroceryMarketAPI/Models/Stock.cs
@@ -0,0 +1,9 @@
+﻿namespace GroceryMarketAPI.Models
+{
+    public class Stock
+    {
+        public Product Product { get; set; }
+        public double WholesalePrice { get; set; }
+        public int WholesaleCount { get; set; }
+    }
+}
---
+++ GroceryMarketAPI/ModelsDTO/StockDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GroceryMarketAPI.ModelsDTO
+{
+    public class StockDto
+    {
+        public ProductDto ProductDto { get; set; }
+        public double WholesalePrice { get; set; }
+        public int WholesaleCount { get; set; }
+    }
+}
